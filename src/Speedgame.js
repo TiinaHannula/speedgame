d45cf91ce73d6fdcae0a9770b36335acac29b9e9
TiinaHannula <50195929+TiinaHannula@users.noreply.github.com>
2019-10-29 23:01:51 +0200

    Unohtunut speedgame.

---
+++ src/Speedgame.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import './Speedgame.css';
+
+class Speedgame extends Component {
+    state = {
+    }
+    render () {
+      return (
+        <div className="container">
+        </div>
+      )
+    }
+ }
+
+
+export default Speedgame;
